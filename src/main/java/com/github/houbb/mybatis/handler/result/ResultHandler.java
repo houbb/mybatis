2e96d63523ed0403e2630da5f3147269c8094596
houbb <1060732496@qq.com>
2020-07-07 23:02:25 +0800

    release branch 0.0.9

--- src/main/java/com/github/houbb/mybatis/handler/result/ResultHandler.java
+++ src/main/java/com/github/houbb/mybatis/handler/result/ResultHandler.java
@@ -11,7 +11,9 @@ import com.github.houbb.mybatis.handler.type.handler.TypeHandler;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author binbin.hou
@@ -31,9 +33,18 @@ public class ResultHandler {
      */
     private final Config config;
 
+    /**
+     * 映射 map
+     *
+     * 可以优化
+     * @since 0.0.9
+     */
+    private final Map<String, Field> fieldMap;
+
     public ResultHandler(Class<?> resultType, Config config) {
         this.resultType = resultType;
         this.config = config;
+        this.fieldMap = buildClassFieldMap();
     }
 
     /**
@@ -52,10 +63,19 @@ public class ResultHandler {
             // 结果大小的判断
             // 为空直接返回，大于1则报错
             if(resultSet.next()) {
-                List<Field> fieldList = ClassUtil.getAllFieldList(resultType);
 
-                for(Field field : fieldList) {
-                    Object value = getResult(field, resultSet);
+                // 列数的总数
+                int columnCount = resultSet.getMetaData().getColumnCount();
+
+                for(int i = 1; i <= columnCount; i++) {
+                    String columnName = resultSet.getMetaData().getColumnName(i);
+                    Field field = fieldMap.get(columnName);
+                    //不存在，则报错
+                    if(field == null) {
+                        throw new MybatisException("No class field match for column: " + columnName);
+                    }
+
+                    Object value = getResult(field, columnName, resultSet);
 
                     // 不为 null 才进行设置
                     if(ObjectUtil.isNotNull(value)) {
@@ -74,17 +94,45 @@ public class ResultHandler {
     }
 
     /**
+     * 获取映射字段
+     * @return map
+     * @since 0.0.9
+     */
+    private Map<String, Field> buildClassFieldMap() {
+        List<Field> fieldList = ClassUtil.getAllFieldList(resultType);
+
+        Map<String, Field> map = new HashMap<>();
+
+        for(Field field : fieldList) {
+            String fieldName = field.getName();
+            // 驼峰转下划线，后期这里应该可以配置，或者指定注解。
+            String columnName = StringUtil.camelToUnderline(fieldName);
+            map.put(columnName, field);
+        }
+
+        return map;
+    }
+
+    /**
      * 获取对应的结果
-     * @param field 子弹
+     *
+     * TODO: 这里其实有几种方式优雅的获取 rs 查询的列信息
+     *
+     * 1. rs.findColumn() 经过测试，发现会报错，不友好
+     *
+     * 2. 解析 sql，构建 rs 的字段集合。
+     *
+     * 3. 根据 {@link ResultSet#getMetaData()} 处理信息
+     *
+     * 此处暂时使用第三种方式。
+     * @param field 字段信息
+     * @param columnName 列名称
      * @param rs 结果集
      * @return 结果
      * @since 0.0.1
      */
-    public Object getResult(Field field, ResultSet rs) {
+    public Object getResult(Field field, String columnName, ResultSet rs) {
         try {
-            String fieldName = field.getName();
-            // 驼峰转下划线，后期这里应该可以配置，或者指定注解。
-            String columnName = StringUtil.camelToUnderline(fieldName);
             Class<?> type = field.getType();
 
             TypeHandler<?> typeHandler = config.getTypeHandler(type);
